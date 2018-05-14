aebf6f0170051ecc278b863afb88f846e383ab0e
Rahul Tallamraju <rahul.t@research.iiit.ac.in>
2018-05-15 00:41:57 +0200

    Working Battery and Actuator Plugins. pending ROS interface of Actuator with Battery. Namespace issues to be resolved

---
+++ minion_robot/scripts/cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# stopping all rosnodes
+rosnode kill --all
+# stopping the gazebo client aka gui
+killall gzclient
+# stopping the gazebo server
+killall gzserver
+killall -9 roslaunch
+killall -9 roslaunch
+killall -9 roslaunch
+
+# lets get a bit more drastic
+# pkill -f ros/indigo
+# pkill -f /home/dementor/catkin_ws
+
