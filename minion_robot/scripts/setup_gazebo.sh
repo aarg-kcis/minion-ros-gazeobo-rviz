4032de99f7be6f635fa8a5ba7a5309e5c059f13c
Rahul Tallamraju <rahul.t@research.iiit.ac.in>
2018-05-18 14:14:51 +0200

    startup and clean

--- minion_robot/scripts/setup_gazebo.sh
+++ minion_robot/scripts/setup_gazebo.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 
-# ROBOS=$1
+ROBOS=$1
 
 # ROBOT_IDS="["
 # HUMAN_INPUT="[1"
 
-# Xs=( -8 -3 -5 -1 3 7 11 15 19 23)
-# Ys=( -8 -8 -8 -8 -8 -8 -8 -8 -8 -8)
+Xs=( -8 -3 -5 -1 3 7 11 15 19 23)
+Ys=( -8 -8 -8 -8 -8 -8 -8 -8 -8 -8)
 
-# if [ $# -ne 1 ]; then
-#         echo "usage: $0 <number of robots>"
-#         exit
-# fi
+if [ $# -ne 1 ]; then
+        echo "usage: $0 <number of robots>"
+        exit
+fi
 
 
 
@@ -20,10 +20,23 @@
 # for i in $(seq 0 $(($ROBOS-1))); do
 # 	id=$(($i+1))
 # 	roslaunch rotors_gazebo mav_with_joy_and_ID.launch roboID:=$id Z:=5 X:=${Xs[$i]}  Y:=${Ys[$i]} --screen &
-# 	rosrun hkt_experiments uav_state_tf_closer $id &         
+# 	rosrun hkt_experiments uav_state_tf_closer $id &
 # done
 
 
-# date
-roslaunch minion_robot controller_gazebo.launch --screen &
-roslaunch minion_robot minion_world.launch  --screen &
+date
+#roslaunch minion_robot gazebo.launch --screen &
+
+#sleep 3
+
+for i in $(seq 0 $(($ROBOS-1))); do
+  id=$(($i+1))
+  roslaunch minion_robot minion_with_ID.launch roboID:=$id z:=0 x:=${Xs[$i]}  y:=${Ys[$i]} --screen &
+  sleep 5
+  #roslaunch minion_robot minion_nodes.launch --screen &
+
+done
+
+
+
+#roslaunch minion_robot minion_battery_plugin.launch  --screen &
